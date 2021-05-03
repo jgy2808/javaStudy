831cb59a92ffc4f9b5fff1ed131b8f343e346301
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-03 21:19:50 +0900

    create project2 add to println

---
+++ project1/src/org/practice/project1/Practiceproject2.java
@@ -0,0 +1,11 @@
+package org.practice.project1;
+
+public class Practiceproject2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is project2");
+		System.out.println("And make git to mine");
+
+	}
+
+}
