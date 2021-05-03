f5836cf5dbf0b92251255ca474611eeb197a1a57
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-03 21:15:59 +0900

    println Hello World

--- project1/src/org/practice/project1/Praticeproject1.java
+++ project1/src/org/practice/project1/Praticeproject1.java
@@ -3,12 +3,7 @@ package org.practice.project1;
 public class Praticeproject1 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 10; i++) {
-			for (int j = 2; j < 10; j++) {
-				System.out.printf("%d * %d = %2d ", j, i, (i*j));
-			}
-			System.out.println();
-		}
+		System.out.println("Hello World!");
 	}
 
 }
