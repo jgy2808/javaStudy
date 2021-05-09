4457858c16c0f9b075dcc828c6b8b39f8cf30125
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-10 02:04:56 +0900

    java for practice print alphabet ranking

---
+++ project2/src/org/practice/project2/Practiceproject3.java
@@ -0,0 +1,12 @@
+package org.practice.project2;
+import java.util.Scanner;
+public class Practiceproject3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		
+
+	}
+
+}
