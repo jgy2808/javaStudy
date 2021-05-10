b9f6ed129e55691d8a9863a5b0e7c47ea406c316
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-10 23:43:30 +0900

    java for practice by print alphabet

--- project2/src/org/practice/project2/Practiceproject3.java
+++ project2/src/org/practice/project2/Practiceproject3.java
@@ -5,7 +5,12 @@ public class Practiceproject3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char c = sc.next().charAt(0);
-		
+		while(c >= 'a') {
+			for(char i = 'a'; i <= c; i++ ) {
+				System.out.print(i);
+			}System.out.println();
+			c--;
+		}
 
 	}
 
