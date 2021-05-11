25708015077a13b2546e10fac8ad7e46836a5d90
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-11 12:40:27 +0900

    update main while

--- project3/src/org/practice/project3/WordGameApp.java
+++ project3/src/org/practice/project3/WordGameApp.java
@@ -27,10 +27,21 @@ public class WordGameApp {
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		String name, word; int num;
+		System.out.print("게임에 참가하는 인원은 몇명입니까>>");
+		num = sc.nextInt();
 		Player[] p = new Player[num];
 		for (int i = 0; i < num; i++) {
-			
+			System.out.print("참가자의 이름을 입력하세요>>");
+			name = sc.nextLine();
+			p[i].setName(name);
+		}
+		System.out.println("시작하는 단어는 아버지입니다");
+		int i = 0;
+		word = "아버지";
+		while(true) {
+			p[i].SayWord();
+			p[i].Succeed();
 		}
 	}
 
