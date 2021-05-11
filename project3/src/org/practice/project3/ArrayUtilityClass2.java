8e18cbbd7c89c748a0f03adb224e41378b3a1943
niceg <niceg@LAPTOP-3FJU39M7>
2021-05-11 20:49:35 +0900

    ArrayUtility2 remove method incomplete

---
+++ project3/src/org/practice/project3/ArrayUtilityClass2.java
@@ -0,0 +1,24 @@
+package org.practice.project3;
+
+public class ArrayUtilityClass2 {
+	public static class ArrayUtility{
+		static int[] concat(int[] s1, int[] s2) {
+			int[] arr = new int[s1.length + s2.length];
+			arr = s1;
+			for (int i = s1.length; i < s2.length; i++) {
+				arr[i] = s2[i - s1.length];
+			}
+			return arr;
+			
+		}
+		static int[] remove(int[] s1, int[] s2) {
+			int arr[];
+			arr[0] = 1;
+			return arr;
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
