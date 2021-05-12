8777f8234419a023b25d27e5cac913a8ba18195a
jgy2808 <jgy2808@naver.com>
2021-05-12 11:51:09 +0900

    ArrayUtility2 while version complete

--- project3/src/org/practice/project3/ArrayUtilityClass2.java
+++ project3/src/org/practice/project3/ArrayUtilityClass2.java
@@ -14,47 +14,29 @@ public class ArrayUtilityClass2 {
 		}
 		static int[] remove(int[] s1, int[] s2) {
 			int cnt = 0;
-			int i = 0, j = 0;
-			int[] arr = s1;
+			int i = 0, j = 0, k = 0;
 			int[] temp;
 			while(i < s1.length) {
-				while(j < s2.length)
-				if (s1[i] == s2[j]) {
-					temp = new int[s1.length - 1];
-					for (int k = 0; k < temp.length - 1; k++) {
-						temp[k] = s1[k + 1];
-					}
-					s1 = new int[temp.length];
-					s1 = temp;
-				}
-			}
-			
-			
-			/*for(int i = 0; i < s1.length; i++) {
-				for (int j : s2) {
-					if (s1[i] == j) {
+				while(j < s2.length) {
+					if (s1[i] == s2[j]) {
 						s1[i] = 0;
+						cnt++;
 						break;
 					}
-					else if(j > s1[i]) {
-						cnt++;	
+					else if (s1[i] < s2[j]) {
 						break;
 					}
+					j++;
+				}
+				i++;
+			}
+			temp = new int[s1.length - cnt];
+			for (i = 0; i < s1.length; i++) {
+				if (s1[i] > 0) {
+					temp[k++] = s1[i];
 				}
 			}
-			System.out.println("cnt : " + cnt);
-			int arr[] = new int[cnt];
-			int k = 0;
-			for (int i = 0; i < cnt; i++) {
-				for (int j = k; j < s1.length; j++) { 
-					if (s1[j] > 0) {
-						arr[i] = s1[j];
-						k = j + 1;
-						break;
-					}
-				}
-			}*/
-			return arr;
+			return temp;
 		}
 	}
 	public static void main(String[] args) {
@@ -71,16 +53,6 @@ public class ArrayUtilityClass2 {
 		for (int i = 0; i < re.length; i++) {
 			System.out.print(re[i] + " ");
 		}System.out.println();
-		int[] s3 = new int[5];
-		System.out.println(s3.length);
-		s3[0] = 1;
-		s3[1] = 2;
-		s3[2] = 3;
-		
-		System.out.println(s3.length);
-		for(int i : s3) {
-			System.out.println(i);
-		}
 		
 	}
 
