8924f40af38415762b0166b2f0edf2ebc5f49493
jgy2808 <jgy2808@naver.com>
2021-05-13 21:55:28 +0900

    create EnumPractice.java

---
+++ project3/src/org/practice/project3/EnumPractice1.java
@@ -0,0 +1,24 @@
+package org.practice.project3;
+
+public class EnumPractice1 {
+	
+	public enum SeatLine{
+		S(1),
+		A(2),
+		B(3);
+		private int value;
+		SeatLine(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			return value;
+		}
+	}
+
+	public static void main(String[] args) {
+		int seat;
+		seat = SeatLine
+		
+	}
+
+}
