58a7d9f51ee946ff4a4482bacdcb09b3d306ae54
jgy2808 <jgy2808@naver.com>
2021-05-14 13:37:33 +0900

    update project before short coding

--- project3/src/org/practice/project3/ReservationProject.java
+++ project3/src/org/practice/project3/ReservationProject.java
@@ -3,36 +3,17 @@ package org.practice.project3;
 import java.util.Scanner;
 
 public class ReservationProject {
-	public static final int NUM_SEAT_TYPE = 3;
-	public static final int NUM_SEAT = 10;
+	public static final int NUM_SEATTYPE = 3;
+	public static final int NUM_MAX_SEAT= 10;
 	static Scanner sc = new Scanner(System.in);
-
-//	public enum SEAT_TYPE {
-//		S(1),
-//		A(2),
-//		B(3);
-//		private int value;
-//		SEAT_TYPE(int value) {
-//			this.value = value;
-//		}
-//	};
+	
+	public enum SEAT_TYPE { S, A, B };
 
 	public static int ChoiceSeat() {
 		int seat;
-		while (true) {
-			try {
-				System.out.print("좌석구분 S<1>, A<2>, B<3> >>");
-				seat = sc.nextInt();
-				if (seat >= 1 && seat <= 3) {
-					break;
-				}
-				System.out.println("다시 입력해주세요.");
-			} catch (Exception e) {
-				sc = new Scanner(System.in);
-				System.out.println("다시 입력해주세요.");	
-			}
-		}
-		return seat;
+		System.out.print("좌석구분 S<1>, A<2>, B<3> >>");
+		seat = sc.nextInt();
+		return seat - 1;
 	}
 	
 	public static class Subscriber {
@@ -44,19 +25,8 @@ public class ReservationProject {
 			seat = ChoiceSeat();
 			System.out.print("이름>>");
 			name = sc.next();
-			while (true) {
-				try {
-					System.out.print("번호>>");
-					number = sc.nextInt();
-					if (number >= 0 && number <= NUM_SEAT) {
-						break;
-					}
-					System.out.println("다시 입력해주세요.");
-				} catch (Exception e) {
-					sc = new Scanner(System.in);
-					System.out.println("다시 입력해주세요.");
-				}
-			}
+			System.out.print("번호>>");
+			number = sc.nextInt();
 		}
 
 		public String getName() {
@@ -68,18 +38,18 @@ public class ReservationProject {
 		}
 
 		public int getNumber() {
-			return number;
+			return number - 1;
 		}
 	}
 
 	public static class SubscriberList {
-		Subscriber s;
-		private Subscriber[][] list = new Subscriber[NUM_SEAT_TYPE][];
+		private Subscriber s;
+		private Subscriber[][] list = new Subscriber[NUM_SEATTYPE][];
 
 		public SubscriberList() {
-			for (int i = 0; i < NUM_SEAT_TYPE; i++) {
-				list[i] = new Subscriber[NUM_SEAT];
-				for (int j = 0; j < NUM_SEAT; j++) {
+			for (int i = 0; i < NUM_SEATTYPE; i++) {
+				list[i] = new Subscriber[NUM_MAX_SEAT];
+				for (int j = 0; j < NUM_MAX_SEAT; j++) {
 					list[i][j] = new Subscriber();
 				}
 			}
@@ -88,70 +58,66 @@ public class ReservationProject {
 		public void AddSubscriber() {
 			s = new Subscriber();
 			s.setSubscriber();
-			if (s.getNumber() == list[s.getSeat() - 1][s.getNumber() - 1].getNumber()) {
+			if (s.getNumber() == list[s.getSeat()][s.getNumber()].getNumber()) {
 				System.out.println("이미 있는 좌석입니다.");
 				return;
 			}
-			list[s.getSeat() - 1][s.getNumber() - 1] = s;
+			list[s.getSeat()][s.getNumber()] = s;
 		}
 
 		public void Viewseat(int seattype) {
-			if (seattype == 1) {
-				System.out.print("S>>");
-			} else if (seattype == 2) {
-				System.out.print("A>>");
-			} else if (seattype == 3) {
-				System.out.print("B>>");
-			}
+			System.out.print(SEAT_TYPE.values()[seattype] + ">>");
 
-			for (int i = 0; i < NUM_SEAT; i++) {
+			for (int i = 0; i < NUM_MAX_SEAT; i++) {
 				System.out.print((i + 1) + ". ");
-				if (list[seattype - 1][i].getName() == null)
+				if (list[seattype][i].getName() == null)
 					System.out.print("_");
 				else
-					System.out.print(list[seattype - 1][i].getName());
-				if (i < NUM_SEAT - 1) System.out.print(" ");
+					System.out.print(list[seattype][i].getName());
+				if (i < NUM_MAX_SEAT - 1) System.out.print(" ");
 			}
 			System.out.println();
 		}
 
 		public void ViewAllSeat() {
-			for (int i = 1; i <= NUM_SEAT_TYPE; i++) {
+			for (int i = 0; i < NUM_SEATTYPE; i++) {
 				Viewseat(i);
 			}
 		}
 
 		public boolean IsAllNull(int seattype) {
-			for (int i = 0; i < NUM_SEAT; i++) {
+			for (int i = 0; i < NUM_MAX_SEAT; i++) {
 				if (list[seattype][i].getName() != null)
 					return false;
 			}
 			return true;
 		}
 
-		public void CancelReserve(int seattype) {
+		public int CancelReserve(int sC) {
 			int num;
+			int seattype = ChoiceSeat();
 			Viewseat(seattype);
-			if (IsAllNull(seattype - 1)) {
+			if (IsAllNull(seattype)) {
 				System.out.println("해당 좌석 타입의 좌석이 모두 비어있습니다.");
-				return;
 			}
 			// -- 이름 말고 번호 입력해서 예약 취소하기 -> 동명이인 있을 수 있음 --
 			System.out.print("번호>>");
 			num = sc.nextInt();
-			if (list[seattype - 1][num - 1].getName() == null) {
+			if (list[seattype][num - 1].getName() == null) {
 				System.out.println("해당 좌석은 비어있습니다.");
 			}
 			else {
-				list[seattype - 1][num - 1] = new Subscriber();
+				list[seattype][num - 1] = new Subscriber();
 				System.out.println("좌석 예약이 취소되었습니다.");
+				return sC - 1;
 			}
+			return sC;
 		}
 	}
 
 	public static void main(String[] args) {
 		SubscriberList subList = new SubscriberList();
-		int sCount = 0;
+		int subscriberCount = 0;
 		int num = 0, k;
 		while (true) {
 			try {
@@ -160,16 +126,19 @@ public class ReservationProject {
 				switch (num) {
 				case 1:
 					subList.ViewAllSeat();
+					if (subscriberCount == 30) {
+						System.out.print("모든 좌석이 꽉 찼습니다.");
+						break;
+					}
 					subList.AddSubscriber();
-					sCount++;
+					subscriberCount++;
 					break;
 				case 2:
 					subList.ViewAllSeat();
 					System.out.println("<<<조회를 완료하였습니다.>>>");
 					break;
 				case 3:
-					k = ChoiceSeat();
-					subList.CancelReserve(k);
+					subscriberCount = subList.CancelReserve(subscriberCount);
 					break;
 				case 4:
 					return;
