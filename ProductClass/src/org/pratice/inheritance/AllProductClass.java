bb2e9af60f488b90b00dffe325cd6b63f3928cd8
jgy2808 <jgy2808@naver.com>
2021-05-15 01:04:48 +0900

    define all class and not programming yet

---
+++ ProductClass/src/org/pratice/inheritance/AllProductClass.java
@@ -0,0 +1,20 @@
+package org.pratice.inheritance;
+
+public class AllProductClass {
+	public static class Product{
+		
+	}
+	public static class CompactDisk extends Product {
+		
+	}
+	public static class Book extends Product {
+		
+	}
+	public static class ConversationBook extends Book {
+		
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
