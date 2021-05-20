ddf27f75d54d53355865c1cd18cb275923e5b57f
jgy2808 <jgy2808@naver.com>
2021-05-20 11:56:24 +0900

    complete alphabet histogram program

--- AlphabetHistogram/src/org/practice/AlphabetHistogram/AlphabetHistogram.java
+++ AlphabetHistogram/src/org/practice/AlphabetHistogram/AlphabetHistogram.java
@@ -16,15 +16,18 @@ public class AlphabetHistogram {
 				{
 					break;
 				}
-				else if (a >= 'a' && a <= 'z') {
+				
+				else if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')) {
+					if (a >= 'A' && a <= 'Z') a += 32;
 					count[a - 'a'] += 1;
 				}
 			}
 		} catch (IOException e) {
 			System.out.println("입력 오류 발생");	
-		}
+		}System.out.println();
+		System.out.println("히스토그램을 그립니다.");
 		for (int i = 0; i < count.length; i++) {
-			System.out.print((char) (i + 65) + ">");
+			System.out.print((char)(i + 65) + ">");
 			for (int j = 0; j < count[i]; j++) {
 				System.out.print("-");
 			} System.out.println();
