6a5d4e077580f7a279e725f07729637710c12073
jgy2808 <jgy2808@naver.com>
2021-05-20 22:00:09 +0900

    word and upper case count, longest word search complete

--- project4/src/org/practice/project4/WordCountClass.java
+++ project4/src/org/practice/project4/WordCountClass.java
@@ -8,22 +8,32 @@ public class WordCountClass {
 	public static void main(String[] args) {
 		InputStreamReader rd = new InputStreamReader(System.in);
 		int upperCount = 0, count = 0, len, max;
+		String biggest = "";
+		String temp = "";
 		try {
 			while(true) {
 				int c = rd.read();
 				if (c == -1) break;
-				if ((c == 32 || c == 13)) {
+				if (c != 32 && c != 13 && c != 10) {
+					temp = temp.concat(String.valueOf((char)c));
+				}
+				if (c == 32 || c == 13) {
+					if (temp.length() > biggest.length()) {
+						biggest = temp;
+					}
+					temp = "";
 					count++;
 				}
 				if (c >= 'A' && c <= 'Z') {
 					upperCount++;
 				}
-				System.out.print((char)c);
+				
 			}
 		} catch (IOException e) {
 			System.out.println("입력 오류 발생");
 		}
-		System.out.println(count);
-		System.out.println(upperCount);
+		System.out.println("w : " + count);
+		System.out.println("u : " + upperCount);
+		System.out.println("b : " + biggest + ", len : " + biggest.length());
 	}
 }
