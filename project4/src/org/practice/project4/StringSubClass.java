e53ede7d4f279e9a0e5a61f47cb0021c3ecac88f
jgy2808 <jgy2808@naver.com>
2021-05-20 16:19:46 +0900

    complete use str.replace(string, string)

--- project4/src/org/practice/project4/StringSubClass.java
+++ project4/src/org/practice/project4/StringSubClass.java
@@ -1,10 +1,11 @@
 package org.practice.project4;
 
+import java.util.Iterator;
+
 public class StringSubClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(args[0].replace("is", "are"));
+		
 	}
 
 }
