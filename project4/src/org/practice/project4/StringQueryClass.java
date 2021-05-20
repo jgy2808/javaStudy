41000d471c9ba08e5a2b4e590b69448970fee9b0
jgy2808 <jgy2808@naver.com>
2021-05-20 23:03:48 +0900

    complete String Query optimization

--- project4/src/org/practice/project4/StringQueryClass.java
+++ project4/src/org/practice/project4/StringQueryClass.java
@@ -5,26 +5,21 @@ public class StringQueryClass {
 	public static void main(String[] args) {
 		String key = "";
 		String val = "";
-		boolean iskey = true;
-		boolean isval = false;
+		String temp = "";
 		for (int i = 0; i < args[0].length(); i++) {
-			if (args[0].charAt(i) != '=' && iskey) {
-				key = key + String.valueOf(args[0].charAt(i));
+			char c = args[0].charAt(i);
+			if (c != '=' && c != '&') {
+				temp += c;
 			}
-			else if (args[0].charAt(i) == '='){
-				iskey = false;
-				isval = true;
+			else if (c == '=') {
+				key = temp;
+				temp = "";
 			}
-			else if (args[0].charAt(i) != '&' && isval) {
-				val = val + String.valueOf(args[0].charAt(i));
-			}
-			if (args[0].charAt(i) == '&' || i == args[0].length()-1){
-				isval = false;
-				iskey = true;
+			if (c == '&' || i == args[0].length() - 1){
+				val = temp;
+				temp = "";
 				System.out.printf("%s\t%s", key, val);
 				System.out.println();
-				key = "";
-				val = "";
 			}
 		}
 	}
