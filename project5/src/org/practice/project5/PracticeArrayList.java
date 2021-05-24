61855c97f0b88bec7af59a7bf9d646db0bea8608
jgy2808 <jgy2808@naver.com>
2021-05-24 23:36:56 +0900

    success test check string in enum

--- project5/src/org/practice/project5/PracticeArrayList.java
+++ project5/src/org/practice/project5/PracticeArrayList.java
@@ -1,24 +1,35 @@
 package org.practice.project5;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class PracticeArrayList {
 	enum Grade{A, B, C, D, F};
+	
+	static boolean InGrade(String s) {
+		for(int i = 0; i < 5; i++) {
+			if (s.compareTo(String.valueOf(Grade.values()[i])) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Character> al = new ArrayList<Character>(5);
-		char c;
+		String c;
 		while(al.size() < 5) {
-			c = sc.next().charAt(0);
-			
-			EnumUtils.isValidEnum(Grade, String.valueOf(c));
-			
-			
-			
-			al.add(c);
-			Grade n = Grade.valueOf(String.valueOf(c));
-			System.out.println(n);
+			c = sc.next();
+			if (InGrade(c)) {
+				al.add(c.charAt(0));
+			}
+			System.out.println(al.size());
+		}
+		Iterator<Character> it = al.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
 		}
 	}
 }
