909a6b30352b877deb90ad891fc39c6587c599c9
jgy2808 <jgy2808@naver.com>
2021-05-25 14:26:37 +0900

    complete vector<Object> practice

--- project5/src/org/practice/project5/GraphicEditor.java
+++ project5/src/org/practice/project5/GraphicEditor.java
@@ -1,5 +1,8 @@
 package org.practice.project5;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 abstract class GraphicObject{
 	private int x;
 	private int y;
@@ -30,10 +33,7 @@ class Rect extends GraphicObject{
 
 	Rect(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
 	}
-
-	@Override
 	public void view() {
 		System.out.println(getX() + ", " + getY() + " -> " + getW() + ", " + getH() + "의 사각형");
 	}
@@ -44,7 +44,6 @@ class Line extends GraphicObject {
 
 	Line(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
 	}
 	public void view() {
 		System.out.println(getX() + ", " + getY() + " -> " + getW() + ", " + getH() + "의 선");
@@ -52,10 +51,24 @@ class Line extends GraphicObject {
 }
 
 public class GraphicEditor {
-
+	
+	Vector<GraphicObject> v = new Vector<GraphicObject>();
+	void add(GraphicObject ob) {
+		v.add(ob);
+	}
+	void draw() {
+		Iterator<GraphicObject> it = v.iterator();
+		while(it.hasNext()) {
+			it.next().view();
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GraphicEditor g = new GraphicEditor();
+		g.add(new Rect(2, 2, 3, 4));
+		g.add(new Line(3, 3, 8, 8));
+		g.add(new Line(2, 5, 6, 6));
+		g.draw();
 	}
 
 }
