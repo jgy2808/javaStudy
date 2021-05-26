f6eb5a8a1dfff2ac0d64b13216c6bc86adabe188
jgy2808 <jgy2808@naver.com>
2021-05-26 22:35:46 +0900

    complete ContentPane example

--- project7/src/org/practice/project7/ContentPaneEx.java
+++ project7/src/org/practice/project7/ContentPaneEx.java
@@ -1,32 +1,16 @@
 package org.practice.project7;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 class NorthPanel extends JPanel {
 	NorthPanel(){
 		setBackground(Color.LIGHT_GRAY);
-		NorthPanel n = new NorthPanel();
-		n.setLayout(new FlowLayout(FlowLayout.LEFT));
-		n.add(new JButton("Open"));
-		n.add(new JButton("Read"));
-		n.add(new JButton("Close"));
 	}
-	
-	
 }
+
 class CenterPanel extends JPanel {
 	CenterPanel(){
-		CenterPanel c = new CenterPanel();
-		c.setLayout(null);
-		c.add(new JLabel("java"));
 	}
 }
 
@@ -34,14 +18,47 @@ public class ContentPaneEx extends JFrame{
 	ContentPaneEx(){
 		setTitle("Open Challenge 9");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
-		c.add(new NorthPanel(), BorderLayout.NORTH);
-		c.add(new CenterPanel(), BorderLayout.CENTER);
-	}
-	public static void main(String[] args) {
 		
+		NorthPanel np = new NorthPanel();
+		np.setLayout(new FlowLayout(FlowLayout.CENTER));
+		np.add(new JButton("Open"));
+		np.add(new JButton("Read"));
+		np.add(new JButton("Close"));
 
+		CenterPanel cp = new CenterPanel();
+		cp.setLayout(null);
+		JLabel jl1 = new JLabel("Java");
+		JLabel jl2 = new JLabel("Hello");
+		JLabel jl3 = new JLabel("Love");
+		jl1.setSize(30, 10);
+		jl1.setOpaque(true);
+		jl1.setBackground(Color.white);
+		jl1.setLocation(30, 30);
+		jl2.setSize(40, 10);
+		jl2.setOpaque(true);
+		jl2.setBackground(Color.white);
+		jl2.setLocation(60, 60);
+		jl3.setSize(30, 10);
+		jl3.setOpaque(true);
+		jl3.setBackground(Color.white);
+		jl3.setLocation(90, 90);
+		cp.add(jl1);
+		cp.add(jl2);
+		cp.add(jl3);
+		
+		c.add(np, BorderLayout.NORTH);
+		c.add(cp, BorderLayout.CENTER);
+		
+		setSize(300, 300);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ContentPaneEx();
+		
 	}
 
 }
