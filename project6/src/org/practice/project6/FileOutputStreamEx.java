1f87c96f4eb9e82ad39c380ef99906dda2950c59
jgy2808 <jgy2808@naver.com>
2021-05-26 15:19:06 +0900

    complete practice file output stream example

--- project6/src/org/practice/project6/FileOutputStreamEx.java
+++ project6/src/org/practice/project6/FileOutputStreamEx.java
@@ -1,10 +1,24 @@
 package org.practice.project6;
-
+import java.io.*;
 public class FileOutputStreamEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			FileOutputStream fout = new FileOutputStream("C:\\Temp/test.txt");
+			FileInputStream fin = null;
+			for (int i = 0; i < 10; i++) {
+				int n = 10 - i;
+				fout.write(n);
+			}
+			fout.close();
+			fin = new FileInputStream("C:\\Temp/test.txt");
+			int c = 0;
+			while ((c = fin.read()) != -1) {
+				System.out.print(c);
+			}
+			fin.close();
+		} catch (IOException e) {
+			System.out.println(e + " 입출력 오류");
+		}
 	}
-
 }
