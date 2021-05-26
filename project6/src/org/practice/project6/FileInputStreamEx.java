cf57403a3e471b4dbd6c3b94831edaa08692afbd
jgy2808 <jgy2808@naver.com>
2021-05-26 12:41:15 +0900

    practice FileInputStream class complete

--- project6/src/org/practice/project6/FileInputStreamEx.java
+++ project6/src/org/practice/project6/FileInputStreamEx.java
@@ -1,12 +1,21 @@
 package org.practice.project6;
 
-
+import java.io.*;
 
 public class FileInputStreamEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream("c:\\windows\\system.ini");
+			int c;
+			while( (c = in.read()) != -1) {
+				System.out.print((char)c);
+			}
+			in.close();
+		} catch (IOException e) {
+			System.out.println("입출력 오류");
+		}
 	}
 
 }
