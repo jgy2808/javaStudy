a9e56b2d7c693a8a4012897bf1074c75dab1057a
jgy2808 <jgy2808@naver.com>
2021-06-03 15:33:15 +0900

    complete graphicclipping : use setfocusable, addKeyListener(new KeyAdapter)

--- project10/src/org/practice/project10/GraphicClippingEx.java
+++ project10/src/org/practice/project10/GraphicClippingEx.java
@@ -6,38 +6,20 @@ import java.awt.event.*;
 
 public class GraphicClippingEx extends JFrame {
 	Container c;
-	ImagePanel ip = new ImagePanel();
 	public GraphicClippingEx() {
 		setTitle("Graphic Clipping Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		c = getContentPane();
-		c.setLayout(new FlowLayout());
-		c.add(ip);
-		ip.addKeyListener(new MyKeyListener());
-		ip.requestFocus();
 		
+		ImagePanel ip = new ImagePanel();
+		c.add(ip, BorderLayout.CENTER);
+
+		ip.setFocusable(true);
 		setSize(400, 400);
 		setVisible(true);
 		
 	}
 	
-	private class MyKeyListener extends KeyAdapter {
-		public void keyPressed(KeyEvent e) {
-			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-				ip.setX(ip.getX() - 10);
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-				ip.setX(ip.getX() + 10);
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_UP) {
-				ip.setY(ip.getY() - 10);
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-				ip.setY(ip.getY() + 10);
-			}
-			ip.repaint();
-		}
-	}
 	
 	class ImagePanel extends JPanel {
 		ImageIcon icon = new ImageIcon("images/bg1.png");
@@ -45,22 +27,34 @@ public class GraphicClippingEx extends JFrame {
 		int clipX = 50;
 		int clipY = 50;
 		
+		public ImagePanel(){
+			addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent e) {
+					if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+						clipX -= 10;
+						System.out.println("LEFT");
+					}
+					else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+						clipX += 10;
+						System.out.println("RIGHT");
+					}
+					else if (e.getKeyCode() == KeyEvent.VK_UP) {
+						clipY -= 10;
+						System.out.println("UP");
+					}
+					else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+						clipY += 10;
+						System.out.println("DOWN");
+					}
+					repaint();
+				}
+			});
+		}
+		
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.setClip(clipX, clipY, 50, 50);
 			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-		}
-		public void setX(int x) {
-			clipX = x;
-		}
-		public int getX() {
-			return clipX;
-		}
-		public void setY(int y) {
-			clipY = y;
-		}
-		public int getY() {
-			return clipY;
 		}
 	}
 
