b412974fab0b5710271603a9a10c5055962c5751
jgy2808 <jgy2808@naver.com>
2021-06-04 16:37:56 +0900

    complete : ImageFullscreen and image+button and MouseMotionListener

--- project10/src/org/practice/project10/PracticeDragonImage.java
+++ project10/src/org/practice/project10/PracticeDragonImage.java
@@ -13,6 +13,8 @@ public class PracticeDragonImage extends JFrame {
 		c = getContentPane();
 		MyPanel p = new MyPanel();
 		c.add(p);
+		
+		
 		setSize(400, 400);
 		setVisible(true);
 	}
@@ -20,14 +22,26 @@ public class PracticeDragonImage extends JFrame {
 	private class MyPanel extends JPanel {
 		ImageIcon icon = new ImageIcon("images/bg1.png");
 		Image img = icon.getImage();
-		
+		int cX = 300, cY = 300;
 		public MyPanel() {
+			setLayout(new FlowLayout());
+			JButton b = new JButton("Hello");
+			add(b);
 			
+			addMouseMotionListener(new MouseAdapter() {
+				public void mouseDragged(MouseEvent e) {
+					cX = e.getX();
+					cY = e.getY();
+					repaint();
+				}
+			});
 		}
 		
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+			g.drawImage(img, 0, 0, c.getWidth(), c.getHeight(), this);
+			g.setColor(Color.GREEN);
+			g.fillOval(cX, cY, 20, 20);
 		}
 	}
 
