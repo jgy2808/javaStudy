5e78b9154aec6f7f117fe4387148e1530e3e123e
jgy2808 <jgy2808@naver.com>
2021-06-16 15:54:22 +0900

    complete : add shooterPanel use extends JPanel, fillRect

--- project11/src/org/practice/project11/ThreadShootingGameEx.java
+++ project11/src/org/practice/project11/ThreadShootingGameEx.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+class ShooterPanel extends JPanel {
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+	}
+}
+
 class ObjMoveThread extends Thread {
 	JLabel label;
 	public ObjMoveThread(JLabel label) {
@@ -14,7 +22,7 @@ class ObjMoveThread extends Thread {
 		while (true) {
 			label.setLocation(label.getX() - 5, label.getY());
 			if (label.getX() == -50) {
-				label.setLocation(350, 0);
+				label.setLocation(350, 10);
 			}
 			try {
 				sleep(200);
@@ -35,13 +43,20 @@ public class ThreadShootingGameEx extends JFrame {
 		c.setLayout(null);
 		
 		ImageIcon icon = new ImageIcon("images/bg1.png");
+		Image img = icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		icon.setImage(img);
 		JLabel iconLabel = new JLabel();
 		iconLabel.setIcon(icon);
 		iconLabel.setSize(50, 50);
-		iconLabel.setLocation(350, 0);
+		iconLabel.setLocation(350, 10);
 		ObjMoveThread th = new ObjMoveThread(iconLabel);
 		
 		c.add(iconLabel);
+		
+		ShooterPanel shooter = new ShooterPanel();
+		shooter.setSize(50, 50);
+		shooter.setLocation(175, 300);
+		c.add(shooter);
 		
 		setLocation(200, 200);
 		setSize(400, 400);
