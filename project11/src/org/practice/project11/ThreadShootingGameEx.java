1fa792470641ba4987614f2a092044316ff0c29f
jgy2808 <jgy2808@naver.com>
2021-06-16 17:26:32 +0900

    incompletion : bullet move, thread build not complete

--- project11/src/org/practice/project11/ThreadShootingGameEx.java
+++ project11/src/org/practice/project11/ThreadShootingGameEx.java
@@ -12,16 +12,49 @@ class ShooterPanel extends JPanel {
 	}
 }
 
-class ObjMoveThread extends Thread {
+class BulletPanel extends JPanel {
+	int x, y;
+	public void shoot() {
+		this.setLocation(getX(), getY() - 5);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, 6, 6);
+	}
+}
+
+class TargetThread extends Thread {
 	JLabel label;
-	public ObjMoveThread(JLabel label) {
+	BulletPanel bp;
+	public TargetThread(JLabel label, BulletPanel bp) {
 		this.label = label;
+		this.bp = bp;
+	}
+	
+	boolean isMeet() {
+		if (bp.getX() >= label.getX() && bp.getX() <= label.getX() + label.getWidth() &&
+				bp.getY() <= label.getY() + label.getHeight()) {
+			return true;
+		}
+		return false;
 	}
 	
 	public void run() {
 		while (true) {
 			label.setLocation(label.getX() - 5, label.getY());
 			if (label.getX() == -50) {
+				label.setLocation(350, 10);
+			}
+			if (isMeet()) {
+				bp.setLocation(null);
+				
+				try {
+					sleep(1500);
+				} catch (InterruptedException e) {
+					return;
+				}
 				label.setLocation(350, 10);
 			}
 			try {
@@ -33,6 +66,14 @@ class ObjMoveThread extends Thread {
 		}
 	}
 }
+
+class ShootThread extends Thread {
+	
+	public void run() {
+		
+	}
+}
+
 
 public class ThreadShootingGameEx extends JFrame {
 	
@@ -49,18 +90,34 @@ public class ThreadShootingGameEx extends JFrame {
 		iconLabel.setIcon(icon);
 		iconLabel.setSize(50, 50);
 		iconLabel.setLocation(350, 10);
-		ObjMoveThread th = new ObjMoveThread(iconLabel);
+		
+		BulletPanel bp = new BulletPanel();
+		bp.setSize(6, 6);
+		bp.setLocation(400/2 - 3, 400-100 - 6);
+		
+		TargetThread th = new TargetThread(iconLabel, bp);
 		
 		c.add(iconLabel);
+		c.add(bp);
 		
 		ShooterPanel shooter = new ShooterPanel();
+		shooter.setBackground(Color.WHITE);
 		shooter.setSize(50, 50);
-		shooter.setLocation(175, 300);
+		shooter.setLocation(400/2 - shooter.getWidth()/2, 400 - shooter.getHeight() - 50);
 		c.add(shooter);
+		
+		c.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					bp.shoot();
+				}
+			}
+		});
 		
 		setLocation(200, 200);
 		setSize(400, 400);
 		setVisible(true);
+		c.requestFocus();
 		
 		th.start();
 	}
