1e4971ce633210a4b89aa5b7be500f3cc39df532
jgy2808 <jgy2808@naver.com>
2021-07-09 16:27:30 +0900

    T369G2P: Update add Player Label Handler

--- project11/src/org/practice/project11/Thread369GameTwoPlayer.java
+++ project11/src/org/practice/project11/Thread369GameTwoPlayer.java
@@ -5,112 +5,128 @@ import java.awt.*;
 import java.awt.event.*;
 
 class GameLabelPlayer1 extends JLabel {
-	boolean flagOf1p = false;
-	void start(int count) {
-		if (flagOf1p)
-			setText(Integer.toString(count));
-	}
 }
 
 class GameLabelPlayer2 extends JLabel {
-	boolean flagOf2p = false;
-	void start(int count) {
-		if (flagOf2p)
-			setText(Integer.toString(count));
-	}
 }
 
-class GameThreadTwoPlayer extends Thread {
-	boolean flagOfStart;
-	boolean flagOfBtn = false;
+class LabelHandler {
 	int count = 1;
 	char keyCode;
 	
-	GameLabelPlayer1 glp1;
-	GameLabelPlayer2 glp2;
-	JButton btn;
-	
-	public GameThreadTwoPlayer(GameLabelPlayer1 glp1, GameLabelPlayer2 glp2, JButton btn) {
-		this.glp1 = glp1;
-		this.glp2 = glp2;
-		this.btn = btn;
+	GameLabelPlayer1 p1;
+	GameLabelPlayer2 p2;
+	LabelHandler(GameLabelPlayer1 p1, GameLabelPlayer2 p2) {
+		this.p1 = p1;
+		this.p2 = p2;
 	}
 	
-	public void run() {
-		while(true) {
-			setBtnEnabled();
+	boolean flag = false;
+	void play() {
+		if (flag) {
 			if (count % 2 == 0)
-				glp1.start(count);
+				p1.setText(Integer.toString(count));
 			else
-				glp2.start(count);
-			try {
-				sleep(700);
-			} catch (InterruptedException e) { return; }
-			isFinished();
-			if (flagOfStart) count += 1;
-//			if (flagOfStart) {
-//				btn.setEnabled(false);
-//				if (setTurn())
-//					glp1.start(count);
-//				else 
-//					glp2.start(count);
-//				try {
-//					sleep(700);
-//				} catch (InterruptedException e) { return; }
-//				isFinished();
-//				count += 1;
-//			}
-//			else {
-//				btn.setEnabled(true);
-//			}
+				p2.setText(Integer.toString(count));
 		}
 	}
 	
-	void init() {
-		flagOfStart = false;
-		flagOfBtn = false;
-		count = 0;
-		glp1.flagOf1p = false;
-		glp2.flagOf2p = false;
-	}
-	
-	void setBtnEnabled() {
-		if (flagOfBtn)
-			btn.setEnabled(false);
-		else
-			btn.setEnabled(true);
-	}
-	
-	void setFlagOfPlayer() {
-		glp1.flagOf1p = true;
-		glp2.flagOf2p = true;
-	}
-	
-	void setKeyCode(char keyCode) {
-		this.keyCode = keyCode;
-	}
-	
-	void isFinished() {
-		if (count % 2 == 0) {
-			if ( (Distinction() == 1 && (keyCode != 'A' || keyCode != 'a') ) ||
-					(Distinction() == 2 && (keyCode != 'S' || keyCode != 's')) ) {
+	boolean isFinished() {
+		if (Distinction() > 0) {
+			if (count % 2 == 0) {
+				if (Distinction() == 1 && keyCode != 'a') {
+					p1.setText("Fail^");
+				} else if (Distinction() == 2 && keyCode != 's') {
+					p1.setText("Fail^^");
+				}
 				System.out.println("1p fail");
-				init();
-			}
-		} else {
-			if ( (Distinction() == 1 && (keyCode != 'k' || keyCode != 'K') ) ||
-					(Distinction() == 2 && (keyCode != 'L' || keyCode != 'l')) ) {
+				return true;
+			} else {
+				if (Distinction() == 1 && keyCode != 'k') {
+					p2.setText("Fail^");
+				} else if (Distinction() == 2 && keyCode != 'l') {
+					p2.setText("Fail^^");
+				}
 				System.out.println("2p fail");
-				init();
+				return true;
 			}
 		}
+		return false;
 	}
 	
+
 	int Distinction() {
 		int one = ((count%10) > 0 && (count%10)%3 == 0) ? 1 : 0;
 		int ten = (count > 9 && (count/10)%3 == 0) ? 1 : 0;
 		return one+ten;
 	}
+	
+
+	void setKeyCode(char keyCode) {
+		this.keyCode = keyCode;
+	}
+}
+
+class GameThreadTwoPlayer extends Thread {
+	boolean flagOfCnt = false;
+	LabelHandler lh;
+	JButton btn;
+	
+	public GameThreadTwoPlayer(GameLabelPlayer1 p1, GameLabelPlayer2 p2, JButton btn) {
+		lh = new LabelHandler(p1, p2);
+		this.btn = btn;
+	}
+	
+	LabelHandler getlabelhandler() {
+		return lh;
+	}
+	
+	public void run() {
+		while(true) {
+//			setBtnEnabled();
+//			if (count % 2 == 0)
+//				glp1.start(count);
+//			else
+//				glp2.start(count);
+//			try {
+//				sleep(700);
+//			} catch (InterruptedException e) { return; }
+//			isFinished();
+//			if (flagOfStart) count += 1;
+//			---------------------------------------------------------------
+//			if (flagOfBtn) {
+//				if (count % 2 == 0)
+//					glp1.setText(Integer.toString(count));
+//				else 
+//					glp2.setText(Integer.toString(count));
+//				try {
+//					sleep(700);
+//				} catch (InterruptedException e) { e.printStackTrace(); return; }
+//				isFinished();
+//				count += 1;
+//			}
+			lh.play();
+			try {
+				sleep(700);
+			} catch (InterruptedException e) { e.printStackTrace(); return; }
+			if (lh.isFinished()) {
+				init();
+			}
+			if (flagOfCnt) lh.count += 1;
+		}
+	}
+	
+	void sethandlerflag() {
+		lh.flag = true;
+	}
+	
+	void init() {
+		lh.flag = false;
+		flagOfCnt = false;
+		lh.count = 1;
+		btn.setEnabled(true);
+	}
+	
 }
 
 public class Thread369GameTwoPlayer extends JFrame {
@@ -121,51 +137,51 @@ public class Thread369GameTwoPlayer extends JFrame {
 		Container c = getContentPane();
 		c.setLayout(null);
 
-		JLabel p1 = new JLabel("1P : A, S");
+		JLabel l1 = new JLabel("1P : A, S");
+		l1.setSize(80, 40);
+		l1.setLocation(60, 60);
+		l1.setFont(new Font("Gothic", Font.ITALIC, 15));
+		l1.setHorizontalAlignment(JLabel.CENTER);
+		c.add(l1);
+		
+		JLabel l2 = new JLabel("2P : K, L");
+		l2.setSize(80, 40);
+		l2.setLocation(160, 60);
+		l2.setFont(new Font("Gothic", Font.ITALIC, 15));
+		l2.setHorizontalAlignment(JLabel.CENTER);
+		c.add(l2);
+		
+		GameLabelPlayer1 p1 = new GameLabelPlayer1();
 		p1.setSize(80, 40);
-		p1.setLocation(60, 60);
-		p1.setFont(new Font("Gothic", Font.ITALIC, 15));
+		p1.setLocation(60, 100);
+		p1.setBackground(Color.YELLOW);
+		p1.setFont(new Font("Gothic", Font.ITALIC, 20));
+		p1.setOpaque(true);
 		p1.setHorizontalAlignment(JLabel.CENTER);
 		c.add(p1);
 		
-		JLabel p2 = new JLabel("2P : K, L");
+		GameLabelPlayer2 p2 = new GameLabelPlayer2();
 		p2.setSize(80, 40);
-		p2.setLocation(160, 60);
-		p2.setFont(new Font("Gothic", Font.ITALIC, 15));
+		p2.setLocation(160, 100);
+		p2.setBackground(Color.YELLOW);
+		p2.setFont(new Font("Gothic", Font.ITALIC, 20));
+		p2.setOpaque(true);
 		p2.setHorizontalAlignment(JLabel.CENTER);
 		c.add(p2);
-		
-		GameLabelPlayer1 glp1 = new GameLabelPlayer1();
-		glp1.setSize(80, 40);
-		glp1.setLocation(60, 100);
-		glp1.setBackground(Color.YELLOW);
-		glp1.setFont(new Font("Gothic", Font.ITALIC, 20));
-		glp1.setOpaque(true);
-		glp1.setHorizontalAlignment(JLabel.CENTER);
-		c.add(glp1);
-		
-		GameLabelPlayer2 glp2 = new GameLabelPlayer2();
-		glp2.setSize(80, 40);
-		glp2.setLocation(160, 100);
-		glp2.setBackground(Color.YELLOW);
-		glp2.setFont(new Font("Gothic", Font.ITALIC, 20));
-		glp2.setOpaque(true);
-		glp2.setHorizontalAlignment(JLabel.CENTER);
-		c.add(glp2);
 		
 		JButton btn = new JButton("start");
 		btn.setSize(80, 20);
 		btn.setLocation(110, 200);
 		
-		GameThreadTwoPlayer gth = new GameThreadTwoPlayer(glp1, glp2, btn);
+		GameThreadTwoPlayer gth = new GameThreadTwoPlayer(p1, p2, btn);
 		
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				glp1.setText("");
-				glp2.setText("");
-				gth.setFlagOfPlayer();
-				gth.flagOfStart = true;
-				gth.flagOfBtn = true;
+				btn.setEnabled(false);
+				p1.setText("");
+				p2.setText("");
+				gth.flagOfCnt = true;
+				gth.sethandlerflag();
 			}
 		});
 		c.add(btn);
@@ -173,7 +189,7 @@ public class Thread369GameTwoPlayer extends JFrame {
 		c.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				System.out.println(e.getKeyChar());
-				gth.setKeyCode(e.getKeyChar());
+				gth.getlabelhandler().setKeyCode(e.getKeyChar());
 			}
 		});
 		
